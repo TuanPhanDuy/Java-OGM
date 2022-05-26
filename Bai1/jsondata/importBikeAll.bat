05fe326c789224e2d7cfd01f2e4d691ee84c65b1
TuanPhanDuy <tuanbin007@gmail.com>
2022-05-26 21:49:22 +0700

    Java OGM

---
+++ Bai1/jsondata/importBikeAll.bat
@@ -0,0 +1,5 @@
+mongoimport --host localhost --port 27017 --db mydb --collection orders --file orders.json
+mongoimport --host localhost --port 27017 --db mydb --collection products --file products.json
+mongoimport --host localhost --port 27017 --db mydb --collection customers --file customers.json
+
+pause
---
+++ Bai2/importAll.bat
@@ -0,0 +1,3 @@
+mongoimport --host localhost --port 27017 --db mydb --collection employees --file employee.json
+pause
+
